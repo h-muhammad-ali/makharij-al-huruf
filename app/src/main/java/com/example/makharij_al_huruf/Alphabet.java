78e5e5cebad2031986097f08a1127c531a841d88
mali-ai <76814802+mali-ai@users.noreply.github.com>
2021-11-06 20:57:03 +0500

    Some Layouts Designed

---
+++ app/src/main/java/com/example/makharij_al_huruf/Alphabet.java
@@ -0,0 +1,13 @@
+package com.example.makharij_al_huruf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Alphabet extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alphabet);
+    }
+}
