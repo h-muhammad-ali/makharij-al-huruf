39d82f6ebc1d8c9f88c5a402449419cfedb3b43d
mali-ai <76814802+mali-ai@users.noreply.github.com>
2021-11-07 14:37:08 +0500

    Functionality for Practice Added

--- app/src/main/java/com/example/makharij_al_huruf/Alphabet.java
+++ app/src/main/java/com/example/makharij_al_huruf/Alphabet.java
@@ -103,113 +103,195 @@ public class Alphabet extends AppCompatActivity implements View.OnClickListener{
     }
     @Override
     public void onClick(View v){
+        Button button = (Button)v;
         switch (v.getId()){
             case R.id.alif:
                 Intent intent1 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent1.putExtra("image", R.drawable.ic_halqiyah);
+                intent1.putExtra("emission", "Sound produced from the End of Throat");
+                intent1.putExtra("letter", button.getText().toString());
                 startActivity(intent1);
                 break;
             case R.id.ba:
                 Intent intent2 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent2.putExtra("image", R.drawable.ic_shafaviyah);
+                intent2.putExtra("emission", "Inner part of the both lips touch each other");
+                intent2.putExtra("letter", button.getText().toString());
                 startActivity(intent2);
                 break;
             case R.id.ta:
                 Intent intent3 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent3.putExtra("image", R.drawable.ic_niteeyah);
+                intent3.putExtra("emission", "Tip of the tongue touching the base of the front 2 teeth");
+                intent3.putExtra("letter", button.getText().toString());
                 startActivity(intent3);
                 break;
             case R.id.tha:
                 Intent intent4 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent4.putExtra("image", R.drawable.ic_lisaveyah);
+                intent4.putExtra("emission", "Tip of the tongue touching the tip of the frontal 2 teeth");
+                intent4.putExtra("letter", button.getText().toString());
                 startActivity(intent4);
                 break;
             case R.id.jeem:
                 Intent intent5 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent5.putExtra("image", R.drawable.ic_shajariyah_haafiyah);
+                intent5.putExtra("emission", "Tongue touching the center of the mouth roof");
+                intent5.putExtra("letter", button.getText().toString());
                 startActivity(intent5);
                 break;
             case R.id.ha:
                 Intent intent6 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent6.putExtra("image", R.drawable.ic_halqiyah);
+                intent6.putExtra("emission", "Sound produced from the Middle of Throat");
+                intent6.putExtra("letter", button.getText().toString());
                 startActivity(intent6);
                 break;
             case R.id.kha:
                 Intent intent7 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent7.putExtra("image", R.drawable.ic_halqiyah);
+                intent7.putExtra("emission", "Sound produced from the Start of Throat");
+                intent7.putExtra("letter", button.getText().toString());
                 startActivity(intent7);
                 break;
             case R.id.daal:
                 Intent intent8 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent8.putExtra("image", R.drawable.ic_niteeyah);
+                intent8.putExtra("emission", "Tip of the tongue touching the base of the front 2 teeth");
+                intent8.putExtra("letter", button.getText().toString());
                 startActivity(intent8);
                 break;
             case R.id.zaal:
                 Intent intent9 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent9.putExtra("image", R.drawable.ic_lisaveyah);
+                intent9.putExtra("emission", "Tip of the tongue touching the tip of the frontal 2 teeth");
+                intent9.putExtra("letter", button.getText().toString());
                 startActivity(intent9);
                 break;
             case R.id.ra:
                 Intent intent10 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent10.putExtra("image", R.drawable.ic_tarafiyah);
+                intent10.putExtra("emission", "Rounded tip of the tongue and some portion near it touching the base of the frontal 4 teeth");
+                intent10.putExtra("letter", button.getText().toString());
                 startActivity(intent10);
                 break;
             case R.id.za:
                 Intent intent11 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent11.putExtra("image", R.drawable.ic_safariyah);
+                intent11.putExtra("emission", "Tip of the tongue comes between the front top and bottom teeth");
+                intent11.putExtra("letter", button.getText().toString());
                 startActivity(intent11);
                 break;
             case R.id.sin:
                 Intent intent12 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent12.putExtra("image", R.drawable.ic_safariyah);
+                intent12.putExtra("emission", "Tip of the tongue comes between the front top and bottom teeth");
+                intent12.putExtra("letter", button.getText().toString());
                 startActivity(intent12);
                 break;
             case R.id.shin:
                 Intent intent13 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent13.putExtra("image", R.drawable.ic_shajariyah_haafiyah);
+                intent13.putExtra("emission", "Tongue touching the center of the mouth roof");
+                intent13.putExtra("letter", button.getText().toString());
                 startActivity(intent13);
                 break;
             case R.id.sad:
                 Intent intent14 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent14.putExtra("image", R.drawable.ic_safariyah);
+                intent14.putExtra("emission", "Tip of the tongue comes between the front top and bottom teeth");
+                intent14.putExtra("letter", button.getText().toString());
                 startActivity(intent14);
                 break;
             case R.id.zad:
                 Intent intent15 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent15.putExtra("image", R.drawable.ic_shajariyah_haafiyah);
+                intent15.putExtra("emission", "One side of the tongue touching the molar teeth");
+                intent15.putExtra("letter", button.getText().toString());
                 startActivity(intent15);
                 break;
             case R.id.taa:
                 Intent intent16 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent16.putExtra("image", R.drawable.ic_niteeyah);
+                intent16.putExtra("emission", "Tip of the tongue touching the base of the front 2 teeth");
+                intent16.putExtra("letter", button.getText().toString());
                 startActivity(intent16);
                 break;
             case R.id.zaa:
                 Intent intent17 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent17.putExtra("image", R.drawable.ic_lisaveyah);
+                intent17.putExtra("emission", "Tip of the tongue touching the tip of the frontal 2 teeth");
+                intent17.putExtra("letter", button.getText().toString());
                 startActivity(intent17);
                 break;
             case R.id.ayn:
                 Intent intent18 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent18.putExtra("image", R.drawable.ic_halqiyah);
+                intent18.putExtra("emission", "Sound produced from the Middle of Throat");
+                intent18.putExtra("letter", button.getText().toString());
                 startActivity(intent18);
                 break;
             case R.id.gayn:
                 Intent intent19 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent19.putExtra("image", R.drawable.ic_halqiyah);
+                intent19.putExtra("emission", "Sound produced from the Start of Throat");
+                intent19.putExtra("letter", button.getText().toString());
                 startActivity(intent19);
                 break;
             case R.id.fa:
                 Intent intent20 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent20.putExtra("image", R.drawable.ic_shafaviyah);
+                intent20.putExtra("emission", "Tip of the two upper jaw teeth touches the inner part of the lower lip");
+                intent20.putExtra("letter", button.getText().toString());
                 startActivity(intent20);
                 break;
             case R.id.qaf:
                 Intent intent21 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent21.putExtra("image", R.drawable.ic_lahatiyah);
+                intent21.putExtra("emission", "Base of Tongue which is near Uvula touching the mouth roof");
+                intent21.putExtra("letter", button.getText().toString());
                 startActivity(intent21);
                 break;
             case R.id.kaf:
                 Intent intent22 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent22.putExtra("image", R.drawable.ic_lahatiyah);
+                intent22.putExtra("emission", "Portion of Tongue near its base touching the roof of mouth");
+                intent22.putExtra("letter", button.getText().toString());
                 startActivity(intent22);
                 break;
             case R.id.lam:
                 Intent intent23 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent23.putExtra("image", R.drawable.ic_tarafiyah);
+                intent23.putExtra("emission", "Rounded tip of the tongue touching the base of the frontal 8 teeth");
+                intent23.putExtra("letter", button.getText().toString());
                 startActivity(intent23);
                 break;
             case R.id.meem:
                 Intent intent24 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent24.putExtra("image", R.drawable.ic_shafaviyah);
+                intent24.putExtra("emission", "Outer part of both lips touch each other");
+                intent24.putExtra("letter", button.getText().toString());
                 startActivity(intent24);
                 break;
             case R.id.noon:
                 Intent intent25 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent25.putExtra("image", R.drawable.ic_tarafiyah);
+                intent25.putExtra("emission", "Rounded tip of the tongue touching the base of the frontal 6 teeth");
+                intent25.putExtra("letter", button.getText().toString());
                 startActivity(intent25);
                 break;
             case R.id.wow:
                 Intent intent26 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent26.putExtra("image", R.drawable.ic_shafaviyah);
+                intent26.putExtra("emission", "Rounding both lips and not closing the mouth");
+                intent26.putExtra("letter", button.getText().toString());
                 startActivity(intent26);
                 break;
             case R.id.haa:
                 Intent intent27 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent27.putExtra("image", R.drawable.ic_halqiyah);
+                intent27.putExtra("emission", "Sound produced from the End of Throat");
+                intent27.putExtra("letter", button.getText().toString());
                 startActivity(intent27);
                 break;
             case R.id.hamza:
@@ -218,6 +300,9 @@ public class Alphabet extends AppCompatActivity implements View.OnClickListener{
                 break;
             case R.id.ya:
                 Intent intent29 = new Intent(Alphabet.this, AlphabetDetails.class);
+                intent29.putExtra("image", R.drawable.ic_shajariyah_haafiyah);
+                intent29.putExtra("emission", "Tongue touching the center of the mouth roof");
+                intent29.putExtra("letter", button.getText().toString());
                 startActivity(intent29);
                 break;
         }
--- app/src/main/java/com/example/makharij_al_huruf/AlphabetDetails.java
+++ app/src/main/java/com/example/makharij_al_huruf/AlphabetDetails.java
@@ -3,12 +3,28 @@ package com.example.makharij_al_huruf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AlphabetDetails extends AppCompatActivity {
 
+    ImageView imageView;
+    FloatingActionButton floatingActionButton;
+    TextView name;
+    TextView details;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alphabet_details);
+        imageView = findViewById(R.id.makhrijImage);
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+        name = findViewById(R.id.makhrijName);
+        details = findViewById(R.id.makhrijDetails);
+        imageView.setImageResource(getIntent().getExtras().getInt("image"));
+        name.setText(getIntent().getExtras().getString("letter"));
+        details.setText(getIntent().getExtras().getString("emission"));
     }
 }
